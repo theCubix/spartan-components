fce9b7679ba2344d69334dccea7a01c3e4577424
[thecubix] <[timon.forrer@gmail.com]>
2018-12-25 15:09:44 +0100

    Updated the cards showcase a bit

--- src/components/sc-pages/sc-home.tsx
+++ src/components/sc-pages/sc-home.tsx
@@ -19,21 +19,34 @@ export class ScHome {
         </sc-section>
     
         <sc-container>
+          <h2 class="title is-2 has-margin-large">Dead Simple Cards</h2>
           <sc-grid class="two-columns">
-
-            <sc-card imageSrc="https://source.unsplash.com/random/400x231" imageAlt="another one">
-              <h1 slot="title" class="title is-2">Web Components FTW.</h1>
+            <sc-card>
+              <h3 slot="title" class="title is-3 underlined">Your dead simple card title.</h3>
               <p slot="content" class="text no-margin-bottom">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               </p>
             </sc-card>
-
-            <sc-card imageSrc="https://source.unsplash.com/random/400x230" imageAlt="some random image">
-              <h1 slot="title" class="title is-2">Right? So it is I guess</h1>
-              <p slot="content" class="text no-margin-bottom">Absodudely. Lorem ipsum oder so. </p>
+            <sc-card>
+              <h3 slot="title" class="title is-3 underlined">Dead awesome</h3>
+              <p slot="content" class="text no-margin-bottom">Absodudely. Lorem ipsum or so. </p>
             </sc-card>
           </sc-grid>
     
+        </sc-container>
+
+        <sc-container>
+          <h2 class="title is-2 has-margin-large">Coloured Card</h2>
+          <sc-grid class="two-columns">
+            <sc-card coloured>
+              <h3 slot="title">Notice the colour on the side?</h3>
+              <p slot="content" class="text no-margin-bottom">See? Quite a nice touch, isn't it?</p>
+            </sc-card>
+            <sc-card coloured imageSrc="//source.unsplash.com/400x250" imageAlt="A random image from the web">
+              <h3 slot="title">Even works with images.</h3>
+              <p slot="content" class="text no-margin-bottom">Cool cool cool.</p>
+            </sc-card>
+          </sc-grid>
         </sc-container>
       </div>
     )
