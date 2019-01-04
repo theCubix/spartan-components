c42ff9c2120fb763d99dc531be69fae710e726ff
[thecubix] <[timon.forrer@gmail.com]>
2019-01-04 23:17:14 +0100

    Tried to add a cty

--- src/components/sc-pages/sc-home.tsx
+++ src/components/sc-pages/sc-home.tsx
@@ -10,32 +10,33 @@ export class ScHome {
       <div>
         <sc-section isFullheight hasCentered hasNomargin>
           <sc-container>
-            <h1 class="title is-1 has-margin">Spartan Components</h1>
+            <h1 class="title is-1 has-margin">spartan components.</h1>
             <sc-list>
               <li>a components collection</li>
               <li>reduced to the minimum</li>
               <li>with little <sc-tooltip underlined tooltip="And helpful tools :)">sparks of colour</sc-tooltip> </li>
             </sc-list>
+            <a class="underlined cta" href="#helloworld">get to know more</a>
           </sc-container>
         </sc-section>
 
-        <sc-section>
+        <sc-section id="helloworld">
           <sc-container>
-            <h2 class="title is-2 has-margin-large">Dead Simple Cards</h2>
+            <h2 class="title is-2 has-margin-large">dead simple cards</h2>
             <sc-grid class="two-columns">
               <sc-card>
-                <h3 slot="title" class="title is-3 underlined">Your dead simple card title.</h3>
+                <h3 slot="title" class="title is-3 underlined">your dead simple card title</h3>
                 <p slot="content" class="text no-margin-bottom">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  lorem ipsum dolor, sit amet consectetur adipisicing elit
                 </p>
               </sc-card>
               <sc-card>
-                <h3 slot="title" class="title is-3 underlined">Dead awesome</h3>
-                <p slot="content" class="text no-margin-bottom">Absodudely. Lorem ipsum or so. </p>
+                <h3 slot="title" class="title is-3 underlined">dead awesome</h3>
+                <p slot="content" class="text no-margin-bottom">absodudely. lorem ipsum or so</p>
               </sc-card>
               <sc-card>
-                <h3 slot="title" class="title is-3 underlined">Dead awesome</h3>
-                <p slot="content" class="text no-margin-bottom">Absodudely. Lorem ipsum or so. </p>
+                <h3 slot="title" class="title is-3">awesome dad</h3>
+                <p slot="content" class="text no-margin-bottom"><i>oh no</i>, i dad it again</p>
               </sc-card>
             </sc-grid>
           </sc-container>
@@ -43,15 +44,15 @@ export class ScHome {
 
         <sc-section>
           <sc-container>
-            <h2 class="title is-2 has-margin-large">Coloured Card</h2>
+            <h2 class="title is-2 has-margin-large">coloured card</h2>
             <sc-grid class="two-columns">
               <sc-card colouredAccent>
-                <h3 slot="title">Notice the colour on the side?</h3>
-                <p slot="content" class="text no-margin-bottom">See? Quite a nice touch, isn't it?</p>
+                <h3 slot="title">notice the colour on the side?</h3>
+                <p slot="content" class="text no-margin-bottom">see? quite a nice touch, isn't it?</p>
               </sc-card>
               <sc-card colouredAccent imageSrc="//source.unsplash.com/400x250" imageAlt="A random image from the web">
-                <h3 slot="title">Even works with images.</h3>
-                <p slot="content" class="text no-margin-bottom">Cool cool cool.</p>
+                <h3 slot="title">even works with images</h3>
+                <p slot="content" class="text no-margin-bottom"><i>coolcoolcool</i></p>
               </sc-card>
             </sc-grid>
           </sc-container>
