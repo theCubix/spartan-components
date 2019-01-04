765c72259914ed3dbfaaf736497c12654b5530ec
[thecubix] <[timon.forrer@gmail.com]>
2019-01-04 22:20:21 +0100

    section styles are now controllable via props

--- src/components/sc-section/sc-section.tsx
+++ src/components/sc-section/sc-section.tsx
@@ -1,10 +1,23 @@
-import { Component } from '@stencil/core'
+import { Component, Prop } from '@stencil/core'
 
 @Component({
   tag: 'sc-section',
   styleUrl: 'sc-section.scss'
 })
 export class ScSection {
+  @Prop() isFullheight: boolean
+  @Prop() hasCentered: boolean
+  @Prop() hasNomargin: boolean
+
+  hostData() {
+    return {
+      'class': { 
+        'is-fullheight': this.isFullheight,
+        'has-centered': this.hasCentered,
+        'has-nomargin': this.hasNomargin,
+      }
+    }
+  }
   render() {
     return(
       <slot />
